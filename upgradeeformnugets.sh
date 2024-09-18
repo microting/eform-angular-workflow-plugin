ba8d646f749c5e9adad6a792db64f7adee4bc2c0
René Schultz Madsen <rm@microting.dk>
2024-09-18 15:13:32 +0200

    Fixing dependency.

--- upgradeeformnugets.sh
+++ upgradeeformnugets.sh
@@ -7,7 +7,7 @@ if (( "$GIT_STATUS" > 0 )); then
 	cd eFormAPI/Plugins/Workflow.Pn/Workflow.Pn
   CURRENT_NUMBER_OF_COMMITS=`git log --oneline | wc -l`
 
-	PACKAGES=('Microting.eForm' 'Microting.eFormApi.BasePn' 'Microting.eFormWorkflowBase')
+	PACKAGES=('Microting.eForm' 'Microting.eFormApi.BasePn' 'Microting.eFormWorkflowBase' 'Microting.EformAngularFrontendBase')
 	PROJECT_NAME='Workflow.Pn.csproj'
 	REPOSITORY='eform-angular-workflow-plugin'
 
