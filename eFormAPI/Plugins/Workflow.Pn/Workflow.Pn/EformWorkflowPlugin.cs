fa9c4f6531c6463c2b35257b3ead6a863cb80eed
René Schultz Madsen <rm@microting.dk>
2024-12-30 06:44:49 +0100

    Removing options.EnableTracing = true;

--- eFormAPI/Plugins/Workflow.Pn/Workflow.Pn/EformWorkflowPlugin.cs
+++ eFormAPI/Plugins/Workflow.Pn/Workflow.Pn/EformWorkflowPlugin.cs
@@ -121,9 +121,6 @@ namespace Workflow.Pn
                 // This option is recommended for client applications only. It ensures all threads use the same global scope.
                 // If you're writing a background service of any kind, you should remove this.
                 options.IsGlobalModeEnabled = true;
-
-                // This option will enable Sentry's tracing features. You still need to start transactions and spans.
-                options.EnableTracing = true;
             });
 
             string pattern = @"Database=(\d+)_eform-angular-workflow-plugin;";
